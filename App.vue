fb3e2ce9699ea22575c582d5794603f748e88799
WayneHu <waynehu2014@163.com>
2025-10-05 15:16:02 +0800

    251005：尝试引入github资源

--- App.vue
+++ App.vue
@@ -17,5 +17,6 @@
 
 	/* 在线链接会被微信小程序禁止 */
 	/* @import url("//at.alicdn.com/t/c/font_5031409_6p1gl9n42mm.css"); */
-	@import url("static/font/iconfont.css");
+	/* @import url("static/font/iconfont.css"); */
+	@import url("https://raw.githubusercontent.com/Wayne-007/iShop/3d1dc8375ae3d3fecd476a2bde9f196e2559e909/static/font/iconfont.css");
 </style>
